32b70a7451a996aa86aad5740d900a4bbc4d32c0
Jyebin <yebin0322@naver.com>
2024-02-20 17:29:20 +0900

    백준 11726번

--- silver3/num11726.java
+++ silver3/num11726.java
@@ -8,7 +8,7 @@ public class num11726 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] dp = new int[1001]; //n개의 타일을 사용해서 만들 수 있는 조합의 개수를 담을 배열
+        int[] dp = new int[n + 1]; //n개의 타일을 사용해서 만들 수 있는 조합의 개수를 담을 배열
         dp[1] = 1;
         if (n >= 2) {
             dp[2] = 2;
