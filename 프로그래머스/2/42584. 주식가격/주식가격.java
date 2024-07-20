3a128d217bdeda2f50a409f9fff5492146bcbeb6
Jyebin <108725996+Jyebin@users.noreply.github.com>
2024-07-20 17:59:45 +0900

    [level 2] Title: 주식가격, Time: 74.51 ms, Memory: 83.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -5,15 +5,17 @@ class Solution {
         int[] answer = new int[length];
 
         for(int i=0; i<length; i++){
+            int cnt = 0;
             int now = prices[i]; // 비교 대상 값
             Stack<Integer> stack = new Stack<>();
             for(int j=i+1; j<length; j++){ // 현재 값 이후의 값들 비교
-                stack.push(prices[j]); //일단 스택에 넣음
-                if(now > prices[j]){ // 현재 가격이 이후 가격보다 크면 멈춤
+                stack.push(prices[j]);
+                if(now > prices[j]){
                     break;
                 }
             }
-            answer[i] = stack.size();
+            cnt = stack.size();
+            answer[i] = cnt;
         }
         return answer;
     }
