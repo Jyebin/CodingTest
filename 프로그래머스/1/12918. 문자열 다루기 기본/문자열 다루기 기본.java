49023d0cc1d38c2e18dcb7e0cf54bf0bfd4cc87b
Jyebin <108725996+Jyebin@users.noreply.github.com>
2024-08-01 16:08:23 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean solution(String s) {
         boolean answer = true;
-        if(s.length() != 4 && s.length() != 6){ //길이가 4이거나 6이면
+        if(s.length() != 4 && s.length() != 6){
             return false;
         }
         
-        for(int i=0; i<s.length(); i++){ //범위에 맞으면
+        for(int i=0; i<s.length(); i++){
             if('0' > s.charAt(i) || s.charAt(i) > '9'){
                 return false;
             }
