0c110fc5a5a7c401c5c386882d8a4735c824ffa6
Jyebin <108725996+Jyebin@users.noreply.github.com>
2024-08-03 15:41:01 +0900

    [level 2] Title: 전화번호 목록, Time: 305.55 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -3,12 +3,10 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        Arrays.sort(phone_book); //오름차순으로 정렬
+        Arrays.sort(phone_book);
         
         for(int i=0; i<phone_book.length-1; i++){
-            String now = phone_book[i];
-            String next = phone_book[i+1];
-            if(next.startsWith(now)){
+            if(phone_book[i+1].startsWith(phone_book[i])){
                 answer = false;
             }
         }
