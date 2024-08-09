dec48bccb37087f9d8f56c469064457a3c8bd37b
Jyebin <108725996+Jyebin@users.noreply.github.com>
2024-08-09 23:28:56 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 34.25 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -2,16 +2,16 @@ import java.util.*;
 
 class Solution {
     public ArrayList solution(int[] arr, int[][] queries) {
-        
+        ArrayList<Integer> list = new ArrayList<>();
         ArrayList<Integer> answer = new ArrayList<>();
         //s<=i<=e, arr[i]가 가장 작은 것. 단 k보다 커야 함
         for(int i=0; i<queries.length; i++){
-            ArrayList<Integer> list = new ArrayList<>();
             int s = queries[i][0];
             int e = queries[i][1];
             int k = queries[i][2];
             for(int j=s; j<=e; j++){
                 if(arr[j] > k){
+                    System.out.println("원본 배열" + arr[j]);
                     list.add(arr[j]); //우선 list에 저장
                 }
             }
@@ -23,6 +23,10 @@ class Solution {
                 Collections.sort(list);
                 //이 중 가장 작은 것을 반환
                 answer.add(list.get(0));
+                for(int a=0; a<list.size(); a++){
+                    System.out.print("여기부터야 ");
+                    System.out.println(list.get(a));
+                }
             }
         }
         
