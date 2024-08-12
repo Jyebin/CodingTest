3b161eede1c3cdf91d680ac5d80598b41a3f64b5
Jyebin <108725996+Jyebin@users.noreply.github.com>
2024-08-13 01:53:04 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.08 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList solution(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(n);
+        while(n>1){
+            if(n%2 == 0){
+                n = n / 2;
+                list.add(n);
+            }else{
+                n = n * 3 + 1;
+                list.add(n);
+            }
+        }
+        return list;
+    }
+}
