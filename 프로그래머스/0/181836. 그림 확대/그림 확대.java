32fa2861f862d221ac0bcc5865bb08964e8c476c
조예빈 <108725996+Jyebin@users.noreply.github.com>
2024-09-20 15:15:44 +0900

    [level 0] Title: 그림 확대, Time: 4.73 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/0/181836. 그림 확대/그림 확대.java
@@ -1,21 +1,10 @@
-import java.util.*;
-
 class Solution {
-    public ArrayList solution(String[] picture, int k) {
+    public String solution(String[] picture, int k) {
         int len = picture.length;
-        ArrayList<String> list = new ArrayList<>();
         for(int i=0; i<len; i++){
-            String newStr = "";
             String[] str = picture[i].split("");
-            for(int j=0; j<str.length; j++){
-                for(int a=0; a<k; a++){
-                    newStr += str[j];
-                }
-            }
-            for(int j=0; j<k; j++){
-                list.add(newStr);
-            }
+            System.out.println(picture[i]);
         }
-        return list;
+        return "answer";
     }
 }
