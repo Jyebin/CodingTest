e9c689e5fe4368c6bb4169ccfc52cc15dcfbdd7e
조예빈 <108725996+Jyebin@users.noreply.github.com>
2024-09-20 13:46:05 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.17 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,37 +1,28 @@
+import java.util.*;
+
 class Solution {
     public int[][] solution(int n) {
+        int[] dx = {0,1,0,-1};
+        int[] dy = {1,0,-1,0};
+        boolean[][] visited = new boolean[n][n];
+        int x = 0;
+        int y = 0;
+        int num = 1;
+        int way = 0;
         int[][] answer = new int[n][n];
-        int num = 1; //배치할 숫자
-        int rowStart = 0; //행(가로)
-        int rowEnd = n-1;
-        int colStart = 0; //열(세로)
-        int colEnd = n-1;
-        //처음에는 오른쪽 끝까지 이동
-        while(rowStart <= rowEnd && colStart <= colEnd){ //아직 채우지 않은 행과 열이 있는지 판단
-            for(int i=colStart; i<=colEnd; i++){ //세로꺼를 하나하나 채워야 함
-                answer[rowStart][i] = num++; //0번째 행
+        while(num <= Math.pow(n,2)){
+            answer[x][y] = num;
+            visited[x][y] = true;
+            if(x + dx[way] < 0 || x + dx[way] >= n || y + dy[way] < 0 || y + dy[way] >= n || visited[x+dx[way]][y+dy[way]]==true){
+                way++;
+                way %= 4;
             }
-            rowStart++; //행 증가
-            
-            for(int i=rowStart; i<=rowEnd; i++){
-                answer[i][colEnd] = num++; //맨 마지막 가로줄 하나씩 채우기
-            }
-            colEnd --; //한 칸 왼쪽으로
-            
-            if(rowStart <= rowEnd){
-                for(int i=colEnd; i>=colStart; i--){
-                    answer[rowEnd][i] = num++; //세로 채우기
-                }
-                rowEnd --; //한 칸 오른쪽으로
-            }
-            
-            if(colStart <= colEnd){
-                for(int i=rowEnd; i>=rowStart; i--){
-                    answer[i][colStart] = num++; //가로 채우기
-                }
-                colStart ++;
-            }
+            //위의 조건을 모두 통과하는 경우에만 이동
+            x = x + dx[way];
+            y = y + dy[way];
+            num++;
         }
+
         return answer;
     }
 }
