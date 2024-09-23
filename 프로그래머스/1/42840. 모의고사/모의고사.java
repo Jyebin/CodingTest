262f0fcf93bf77f5039da1d9c2b337b104e4025d
조예빈 <108725996+Jyebin@users.noreply.github.com>
2024-09-23 20:34:23 +0900

    [level 1] Title: 모의고사, Time: 0.78 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,38 +1,36 @@
 import java.util.*;
 
 class Solution {
-    public ArrayList<Integer> solution(int[] answers) {
-        //1번 : 1,2,3,4,5 -> 5개
-        //2번 : 2,1,2,3,2,4,2,5 -> 8개
-        //3번 : 3, 3, 1, 1, 2, 2, 4, 4, 5, 5 -> 10개
-        int length = answers.length;
-        int[] num1 = {1,2,3,4,5};
-        int num1Result = 0;
-        int[] num2 = {2,1,2,3,2,4,2,5};
-        int num2Result = 0;
-        int[] num3 = {3,3,1,1,2,2,4,4,5,5};
-        int num3Result = 0;
-        
-        ArrayList<Integer> answer = new ArrayList<>();
-        
-        for(int i=0; i<length; i++){
-            if(num1[i%5] == answers[i]){
-                num1Result++;
+    public ArrayList solution(int[] answers) {
+        int[] one = {1,2,3,4,5}; //5개
+        int[] two = {2,1,2,3,2,4,2,5}; //8개
+        int[] three = {3,3,1,1,2,2,4,4,5,5}; //10개
+        int oneScore = 0;
+        int twoScore = 0;
+        int threeScore = 0;
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == one[i%5]){
+                oneScore ++;
             }
-            if(num2[i%8] == answers[i]){
-                num2Result ++;
+            if(answers[i] == two[i%8]){
+                twoScore ++;
             }
-            if(num3[i%10]==answers[i]){
-                num3Result ++;
+            if(answers[i] == three[i%10]){
+                threeScore ++;
             }
         }
-  
-        int maxResult = Math.max(num1Result, Math.max(num2Result, num3Result));
-        ArrayList<Integer> result = new ArrayList<>();
-        if (num1Result == maxResult) result.add(1);
-        if (num2Result == maxResult) result.add(2);
-        if (num3Result == maxResult) result.add(3);
-        
-        return result;
+        ArrayList<Integer> list = new ArrayList<>();
+        int max = Math.max(oneScore, Math.max(twoScore, threeScore));
+        if(max == oneScore){
+            list.add(1);
+        }
+        if(max == twoScore){
+            list.add(2);
+        }
+        if(max == threeScore){
+            list.add(3);
+        }
+        Collections.sort(list);
+        return list;
     }
 }
