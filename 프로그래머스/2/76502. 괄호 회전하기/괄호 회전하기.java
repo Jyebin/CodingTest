0b20ad98a1231791ae621c54dcbb818548976c5e
조예빈 <108725996+Jyebin@users.noreply.github.com>
2024-09-24 22:44:58 +0900

    [level 2] Title: 괄호 회전하기, Time: 82.71 ms, Memory: 96.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -2,51 +2,36 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        //괄호를 스택에 저장 -> 열리는 괄호면 스택에 1 넣기, 닫히는 괄호면 1 빼기
-        //스택이 비어 있다면 올바른 괄호, 안비어있다면 올바르지 않은 괄호
-        //이중 for문 활용. for문을 이용해서 index 수정
-        int length = s.length();
-        int cnt = 0;
-        
-        for(int i=0; i<length; i++){ //몇 번째 문자열이 맨 처음에 올 것인지를 설정해 주는 반복문
+        //왼쪽으로 회전
+        String[] str = s.split("");
+        int answer = 0;
+        int len = s.length();
+        for(int i=0; i<len; i++){
+            String rotated = s.substring(i) + s.substring(0,i);
             Stack<Character> stack = new Stack<>();
             
-            StringBuffer sb = new StringBuffer();
-            
-            for(int j=i; j<length + i; j++){ //문자열을 순회하는 반복문
-                if(length-1 < j){ //j가 문자열 길이보다 더 커지는 순간에만
-                    sb.append(s.charAt(j-length));
-                }else{
-                    sb.append(s.charAt(j));
-                }
-            }
-            boolean isValid = true;
-            for(int k=0;k<sb.length();k++){
-                char c = sb.charAt(k);
-                if(c=='{' || c == '[' || c=='('){
-                    stack.push(c);
-                }else{
-                    if(stack.isEmpty()){
-                        isValid = false;
-                        break;
+            for(int j=0; j<rotated.length(); j++){
+                if(rotated.charAt(j)=='{' || rotated.charAt(j)=='(' || rotated.charAt(j)=='['){
+                    stack.add(rotated.charAt(j));
+                } else { //pop하는 로직
+                    if(stack.isEmpty()){ //비어있으면 pop할 수 없음
+                        stack.add(rotated.charAt(j)); //스택이 비지 않았다는 것을 알려주기 위해 추가
+                        continue;
                     }
-                    char top = stack.pop(); //스택의 상위 요소를 가져옴
-                    if(c == '}' && top != '{'){
-                        isValid = false;
-                        break;
-                    }else if(c == ']' && top != '['){
-                        isValid = false;
-                        break;
-                    }else if(c == ')' && top != '('){
-                        isValid = false;
+                    if(rotated.charAt(j) == '}' && stack.peek() == '{' || 
+                       rotated.charAt(j) == ')' && stack.peek() == '(' || 
+                       rotated.charAt(j) == ']' && stack.peek() == '['){
+                       stack.pop();
+                    }else{
                         break;
                     }
                 }
             }
-            if(isValid && stack.isEmpty()){
-                cnt++;
+            if(stack.isEmpty()){
+                answer++;
             }
         }
-        return cnt;
+        return answer;
+        
     }
 }
