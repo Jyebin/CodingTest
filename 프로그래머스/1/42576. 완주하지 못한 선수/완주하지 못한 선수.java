2df3a2e5f5027357cb56c7d1c233af271ead63b4
조예빈 <108725996+Jyebin@users.noreply.github.com>
2024-09-27 18:08:27 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 98.22 ms, Memory: 96.7 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,31 +2,35 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
+        //participant : 마라톤에 참여한 선수들의 이름
+        //completion : 완주한 선수들
+        //HashMap의 key를 선수 이름으로, value를 인원으로 설정
         int p = participant.length;
         int c = completion.length;
-        //마라톤에 참여한 선수 : participant
-        //완주한 선수 : completion
-        //완주하지 못한 선수 return
-        //HashMap을 이용하여 선수의 이름을 key값으로, 인원수를 value값으로 설정
-        HashMap<String, Integer> map = new HashMap<>();
-        //for문을 돌며 participant 배열에 있는 이름을 key값으로 설정
-        for(int i=0; i<p; i++){ //참여한 선수 넣기
-            if(map.containsKey(participant[i])){ //중복되는 값이 있으면 value + 1
-                map.put(participant[i], map.get(participant[i])+1);
-            }else{ //중복되는 값이 없으면 1
-                map.put(participant[i],1);
+        String answer = "";
+        
+        HashMap<String, Integer> map = new HashMap<>(); //참가자 명단을 저장할 맵
+        
+        for(int i=0; i<p; i++){
+            if(map.containsKey(participant[i])){
+                map.put(participant[i], map.get(participant[i]) + 1);
+            }else{
+                map.put(participant[i], 1);
             }
         }
-        for(int i=0;i<c;i++){ //map에서 참여한 선수 빼기 -> 1씩 감소 하도록 update해야 함
-            if(map.containsKey(completion[i])){
-                map.put(completion[i], map.get(completion[i])-1);
+        
+        for(int i=0; i<c; i++){
+            if(map.containsKey(completion[i])){ //완주자 목록에 있으면
+                 map.put(completion[i], map.get(completion[i]) - 1); //1감소
             }
         }
-        for(int i=0;i<p;i++){
-            if(map.get(participant[i]) != 0){ //값이 0이 아니면 -> 사람이 한 명 이상 존재하면
-                return participant[i];
+
+        //완주자 명단에 없는 사람을 출력해야함
+        for(int i=0; i<p; i++){
+             if(map.get(participant[i]) != 0){
+                answer = participant[i];
             }
         }
-        return null;
+        return answer;
     }
 }
