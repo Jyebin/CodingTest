128f663f5fadc2068f0879ddb01ff958d639649a
조예빈 <108725996+Jyebin@users.noreply.github.com>
2024-09-28 02:51:49 +0900

    [level 2] Title: 할인 행사, Time: 132.55 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -2,19 +2,35 @@ import java.util.*;
 
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
-        //할인제품은 하루에 하나씩만 구매
-        //원하는 제품과 수량이 할인하는 날짜와 10일 연속으로 일치할 경우에 맞춰서 회원가입
-        //해시맵에 우선 넣어두고, 배열을 탐색하면서 있는지 없는지 보기
-        HashMap<String, Integer> map = new HashMap<>();
-        for(int i=0; i<want.length; i++){
-            if(map.containsKey(want[i])){
-                map.put(want[i], map.get(want[i] + number[i]));
-            }else{
-                map.put(want[i], number[i]);
-            }
+        int answer = 0;
+        Map<String,Integer> wantList = new HashMap();
+        
+        for(int i=0; i<want.length;i++){
+            wantList.put(want[i],number[i]);
         }
         
-        int answer = 0;
+       for(int i =0; i<=discount.length-10;i++){
+           Map<String,Integer> map = new HashMap();
+           for(int j=i; j<i+10; j++){
+               if(wantList.containsKey(discount[j])){
+                    map.put(discount[j], map.getOrDefault(discount[j],0)+1);
+                }else{
+                   continue;
+               }
+           }
+           boolean check = true;
+            for(String s : wantList.keySet()){
+                if(!map.containsKey(s) || map.get(s) < wantList.get(s)){
+                    check = false;
+                    break;
+                }
+            }
+            if(check){
+                answer++;
+            }
+           
+       }
+        
         return answer;
     }
 }
