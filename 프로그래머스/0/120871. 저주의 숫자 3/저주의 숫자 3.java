a4735f503e21b80ae602a04779593cd9851c6032
조예빈 <108725996+Jyebin@users.noreply.github.com>
2025-03-06 23:44:20 +0900

    [level 0] Title: 저주의 숫자 3, Time: 8.82 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -1,12 +1,15 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        //3의 배수가 나오면 그 숫자 + 1
-        //숫자는 계속 1씩 더해주어야 함 -> for문을 통해서 1씩 더해주고, 조건에 맞으면 1을 더 더해줌
-        for(int i=0; i<n; i++){
+        //최종적으로 과거의 숫자에다가 1을 더해서 주어진 수(n)까지 가야 함
+        String answerStr = "";
+        for(int i=1; i<=n; i++){
             answer = answer + 1;
-            while(answer % 3 == 0 || String.valueOf(answer).contains("3")){
+            answerStr = answer + "";
+            //만약 3의 배수이면 1을 더 더함
+            while(answer % 3 == 0 || answerStr.contains("3")){ //3의 배수이고 3이라는 숫자가 포함되어 있지 않을 때까지
                 answer = answer + 1;
+                answerStr = answer + "";
             }
         }
         return answer;
