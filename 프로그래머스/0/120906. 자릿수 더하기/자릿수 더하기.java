6580ce4f5855b9958c4553c992ef4c20ded145bd
조예빈 <108725996+Jyebin@users.noreply.github.com>
2025-03-16 21:27:27 +0900

    [level 0] Title: 자릿수 더하기, Time: 6.44 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,11 @@
 class Solution {
     public int solution(int n) {
         //n의 각 자리 숫자의 합 return
-        String input = n + "";
         int answer = 0;
-        for(int i=0; i<input.length(); i++){
-            answer += Integer.parseInt(Character.toString(input.charAt(i)));
+        String nStr = n + "";
+        String[] arr = nStr.split("");
+        for(int i=0; i<arr.length; i++){
+            answer += Integer.parseInt(arr[i]);
         }
         return answer;
     }
