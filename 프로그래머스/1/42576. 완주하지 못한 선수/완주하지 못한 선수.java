718f4ce5fa1f70d6658455f15ecc411d970ed7a0
조예빈 <108725996+Jyebin@users.noreply.github.com>
2025-03-19 17:41:57 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 100.46 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,35 +2,31 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        //participant : 마라톤에 참여한 선수들의 이름
-        //completion : 완주한 선수들
-        //HashMap의 key를 선수 이름으로, value를 인원으로 설정
-        int p = participant.length;
-        int c = completion.length;
-        String answer = "";
+        //참여 선수 명단에서 완주한 선수 빼기
+        HashMap<String, Integer> map = new HashMap<>();
         
-        HashMap<String, Integer> map = new HashMap<>(); //참가자 명단을 저장할 맵
-        
-        for(int i=0; i<p; i++){
+        for(int i=0; i<participant.length; i++){
             if(map.containsKey(participant[i])){
                 map.put(participant[i], map.get(participant[i]) + 1);
-            }else{
+            } else {
                 map.put(participant[i], 1);
             }
         }
         
-        for(int i=0; i<c; i++){
-            if(map.containsKey(completion[i])){ //완주자 목록에 있으면
-                 map.put(completion[i], map.get(completion[i]) - 1); //1감소
+        for(int i=0; i<completion.length; i++){
+            if(map.get(completion[i]) >= 0){
+                map.put(completion[i], map.get(completion[i]) - 1);
             }
         }
-
-        //완주자 명단에 없는 사람을 출력해야함
-        for(int i=0; i<p; i++){
-             if(map.get(participant[i]) != 0){
+        
+        String answer = "";
+        
+        for(int i=0; i<participant.length; i++){
+            if(map.get(participant[i]) != 0){
                 answer = participant[i];
             }
         }
+        
         return answer;
     }
 }
