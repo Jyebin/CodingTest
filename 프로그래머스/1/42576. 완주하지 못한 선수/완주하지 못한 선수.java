281a8396d9771923e6e267a2df37a6dd94b3521b
조예빈 <108725996+Jyebin@users.noreply.github.com>
2025-05-14 00:16:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 83.50 ms, Memory: 97.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,31 +2,35 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        //참여 선수 명단에서 완주한 선수 빼기
+        String answer = "";
+        //participant : 참여 선수
+        //completion : 완주한 선수
+        //완주하지 못한 선수 return
+        //hashmap을 통해 key값을 선수 이름으로 하고, value값을 숫자로 해서 완주한 경우에는 -1 빼기
+        //최종적으로 hashmap을 순회해서 return해주기
         HashMap<String, Integer> map = new HashMap<>();
-        
-        for(int i=0; i<participant.length; i++){
-            if(map.containsKey(participant[i])){
+        int length = participant.length;
+        for(int i=0; i<length; i++){
+            //참여 선수 전부를 순회해서 hashmap에 등록
+            if(map.containsKey(participant[i])){ //이미 이름이 있는 경우
                 map.put(participant[i], map.get(participant[i]) + 1);
-            } else {
+            }else{
                 map.put(participant[i], 1);
             }
         }
-        
+        //완주 선수 전부를 순회해서 hashmap에서 제거
         for(int i=0; i<completion.length; i++){
-            if(map.get(completion[i]) >= 0){
-                map.put(completion[i], map.get(completion[i]) - 1);
+            if(map.get(completion[i]) > 0){
+                map.put(completion[i], map.get(completion[i])-1);
             }
         }
         
-        String answer = "";
-        
-        for(int i=0; i<participant.length; i++){
-            if(map.get(participant[i]) != 0){
+        for(int i=0; i<length; i++){
+            if(map.get(participant[i]) == 1){
                 answer = participant[i];
             }
         }
-        
+       
         return answer;
     }
 }
