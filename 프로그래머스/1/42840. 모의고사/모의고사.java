e07740873d6197e17b99ae124e6390319e08117f
조예빈 <108725996+Jyebin@users.noreply.github.com>
2025-05-18 22:59:50 +0900

    [level 1] Title: 모의고사, Time: 5.25 ms, Memory: 86.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,36 +1,61 @@
-import java.util.*;
+import java.util.ArrayList;
 
 class Solution {
+    // 가장 많이 맞춘 사람 -> dfs
     public ArrayList solution(int[] answers) {
-        int[] one = {1,2,3,4,5}; //5개
-        int[] two = {2,1,2,3,2,4,2,5}; //8개
-        int[] three = {3,3,1,1,2,2,4,4,5,5}; //10개
-        int oneScore = 0;
-        int twoScore = 0;
-        int threeScore = 0;
+        //1번 : 1~5 반복
+        //2번 : 2,1,2,3,2,4,2,5 반복
+        //3번 : 3,3,1,1,2,2,4,4,5,5 반복
+        int[] first = {1,2,3,4,5};
+        int[] second = {2,1,2,3,2,4,2,5};
+        int[] third = {3,3,1,1,2,2,4,4,5,5};
+        
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>();
+        ArrayList<Integer> answer = new ArrayList<>();
+        
         for(int i=0; i<answers.length; i++){
-            if(answers[i] == one[i%5]){
-                oneScore ++;
+            list1.add(first[i%5]);
+            list2.add(second[i%8]);
+            list3.add(third[i%10]);
+        }
+        
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int cnt3 = 0;
+        
+        for(int i=0; i<answers.length; i++){
+            if(answers[i] == list1.get(i)){
+                cnt1++;
             }
-            if(answers[i] == two[i%8]){
-                twoScore ++;
+            if(answers[i] == list2.get(i)){
+                cnt2++;
             }
-            if(answers[i] == three[i%10]){
-                threeScore ++;
+            if(answers[i] == list3.get(i)){
+                cnt3++;
             }
         }
-        ArrayList<Integer> list = new ArrayList<>();
-        int max = Math.max(oneScore, Math.max(twoScore, threeScore));
-        if(max == oneScore){
-            list.add(1);
+        if(cnt1 == cnt2 && cnt2 == cnt3){
+            answer.add(1);
+            answer.add(2);
+            answer.add(3);
+        }else if(cnt1 == cnt2 && cnt1 > cnt3){
+            answer.add(1);
+            answer.add(2);
+        }else if(cnt1 == cnt3 && cnt1 > cnt2){
+            answer.add(1);
+            answer.add(3);
+        }else if(cnt2 == cnt3 && cnt2 > cnt1){
+            answer.add(2);
+            answer.add(3);
+        }else if(Math.max(Math.max(cnt1, cnt2), cnt3) == cnt1){
+            answer.add(1);
+        }else if(Math.max(Math.max(cnt1, cnt2), cnt3) == cnt2){
+            answer.add(2);
+        }else if(Math.max(Math.max(cnt1, cnt2), cnt3) == cnt3){
+            answer.add(3);
         }
-        if(max == twoScore){
-            list.add(2);
-        }
-        if(max == threeScore){
-            list.add(3);
-        }
-        Collections.sort(list);
-        return list;
+        return answer;
     }
 }
