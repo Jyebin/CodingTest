3dae280574f2d1a8af182a789ae46614a3f42cab
조예빈 <108725996+Jyebin@users.noreply.github.com>
2025-06-03 17:49:44 +0900

    [level 2] Title: 오픈채팅방, Time: 174.18 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,25 +1,41 @@
+import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.*;
 
 class Solution {
     public ArrayList solution(String[] record) {
-        HashMap<String, String> map = new HashMap<>();
-        ArrayList<String> answer = new ArrayList<>();
+        //닉네임 변경법 : 채팅방 나가고 새로운 닉네임으로 들어감
+        //채팅방에서 닉네임 변경 -> 기존 내용도 전부 변경
+        HashMap<String, String> map = new HashMap<>(); //<uid, 이름>
+        ArrayList<String> list = new ArrayList<>();
         
         for(int i=0; i<record.length; i++){
-            String[] input = record[i].split(" ");
-            if(input[0].equals("Enter") || input[0].equals("Change")){ //우선 닉네임을 다 변경하고 나중에 출력해야 함
-                map.put(input[1], input[2]);
-            }
-        }
+            String[] str = record[i].split(" ");
             
-        for(int i=0; i<record.length; i++){
-            String[] input = record[i].split(" ");
-            if(input[0].equals("Enter")){
-                answer.add(map.get(input[1]) + "님이 들어왔습니다.");
-            }else if(input[0].equals("Leave")){
-                answer.add(map.get(input[1]) + "님이 나갔습니다."); 
+            if(str.length == 3){ //이름 바꾸는 경우
+                String entry = str[0];
+                String uid = str[1];
+                String name = str[2];
+                if(entry.equals("Enter") || entry.equals("Change")){
+                    map.put(uid, name);
+                }
             }
         }
-        return answer;
+        
+        for(int i=0; i<record.length; i++){
+            String[] str = record[i].split(" ");
+            if(str.length < 3){
+                String entry = str[0];
+                String uid = str[1];
+                list.add(map.get(uid) + "님이 나갔습니다.");
+            }else{
+                String entry = str[0];
+                String uid = str[1];
+                if(entry.equals("Enter")){
+                   list.add(map.get(uid) + "님이 들어왔습니다.");
+                }
+            }
+        }
+        return list;
     }
 }
